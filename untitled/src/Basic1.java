6013fb1add008a347b52b4bd15d29811ad0cee49
Sivakoti Sai Deekshitha <Sivakoti.Deekshitha@ivycomptech.com>
2022-08-07 20:14:51 +0530

    second change from branch1

--- untitled/src/Basic1.java
+++ untitled/src/Basic1.java
@@ -1,6 +1,7 @@
 public class Basic1 {
     public static void main(String[] args){
-        System.out.println("Hello !")
+        System.out.println("Hello !");
     }
 }
 //from branch1
+//second change from branch-1
